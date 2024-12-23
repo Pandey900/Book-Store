83b55a331ee5f28fff24fbaf66d25fd2ddb635c7
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-12-24 00:47:40 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/login", userInfo)
+      .post("https://book-store-app-omega-dusky.vercel.app/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
